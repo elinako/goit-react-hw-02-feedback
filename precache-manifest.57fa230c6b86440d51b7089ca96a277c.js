db561493e64e89000d9a1c44c395660873ca66ad
Elina <elisonfog@gmail.com>
2020-09-29 23:52:27 +0300

    Updates

---
+++ precache-manifest.57fa230c6b86440d51b7089ca96a277c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245cc96a14d4c3a4463266a463835ea3",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "239859f7673be727c591",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c58d9634.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c58d9634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48de562f3afafae6bec1",
+    "url": "/goit-react-hw-02-feedback/static/js/main.9458e34b.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
